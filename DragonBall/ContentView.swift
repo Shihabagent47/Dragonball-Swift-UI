ff4e4066cb01b5b4940ae1e882deaf8104d1724b
Shihab Hossain <shihab.hossain@cognate.agency>
2026-05-06 14:12:30 +0600

    Initial Commit

---
+++ DragonBall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DragonBall
+//
+//  Created by Shihab Hossain on 6/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragonBall/DragonBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBallApp.swift
+//  DragonBall
+//
+//  Created by Shihab Hossain on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
